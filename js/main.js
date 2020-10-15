09cceac9cc4fe2b264c7ddf157882e09df39c61a
munjucho-corder <dailymunju@naver.com>
2020-10-15 16:39:30 +0900

    20201015

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready( function(){
+    /**
+     * @bref menu click event
+     */
+    $('#menu_btn,.close_btn').click( function(){
+        $('aside,.modal_background').toggle();
+    });
+}); 
